1a34ae0b6e3181d9d2a5066ae43af8765da84219
aaungdev <dhev.code@gmail.com>
2024-06-23 21:52:54 -0400

    create post

--- frontend/posts/posts.js
+++ frontend/posts/posts.js
@@ -3,6 +3,13 @@
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsZXgxMjM0IiwiaWF0IjoxNzE5MTU3NDkwLCJleHAiOjE3MTkyNDM4OTB9.bNlxL8YTydqW-g3fHPvvpGvtmRDSjksSkHR_mbdb49A";
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchPosts();
+
+  const createPostForm = document.querySelector(".createPostInput");
+  createPostForm.addEventListener("submit", createPost);
+});
+
 async function fetchPosts() {
   try {
     const response = await fetch(
@@ -40,16 +47,14 @@ function displayPosts(posts) {
     postElement.classList.add("post");
 
     const postAuthor = `
-            <article class="postAuthor">
-                <img src="images/user.png" alt="User">
-                <article>
-                    <h1>${post.username}</h1>
-                    <small>${post.bio || ""}</small>
-                    <small>${new Date(
-                      post.createdAt
-                    ).toLocaleTimeString()}</small>
-                </article>
-            </article>`;
+      <article class="postAuthor">
+          <img src="images/user.png" alt="User">
+          <article>
+              <h1>${post.username}</h1>
+              <small>${post.bio || ""}</small>
+              <small>${new Date(post.createdAt).toLocaleTimeString()}</small>
+          </article>
+      </article>`;
 
     const postContent = `<p>${post.text}</p>`;
     const postImage = post.image
@@ -57,40 +62,40 @@ function displayPosts(posts) {
       : "";
 
     const postStats = `
-            <article class="postStats">
-                <article>
-                    <img src="images/like.svg" alt="Like">
-                    <img src="images/love.svg" alt="Love">
-                    <img src="images/celebrate.svg" alt="Celebrate">
-                    <img src="images/support.svg" alt="Support">
-                    <img src="images/insightful.svg" alt="Insightful">
-                    <img src="images/funny.svg" alt="funny">
-                    <span class="likedUser">${post.likes.length} likes</span>
-                </article>
-                <article>
-                    <span>${
-                      post.comments || 0
-                    } comments</span> <b>&nbsp;-&nbsp;</b> <span>${
+      <article class="postStats">
+          <article>
+              <img src="images/like.svg" alt="Like">
+              <img src="images/love.svg" alt="Love">
+              <img src="images/celebrate.svg" alt="Celebrate">
+              <img src="images/support.svg" alt="Support">
+              <img src="images/insightful.svg" alt="Insightful">
+              <img src="images/funny.svg" alt="funny">
+              <span class="likedUser">${post.likes.length} likes</span>
+          </article>
+          <article>
+              <span>${
+                post.comments || 0
+              } comments</span> <b>&nbsp;-&nbsp;</b> <span>${
       post.shares || 0
     } shares</span>
-                </article>
-            </article>`;
+          </article>
+      </article>`;
 
     const postActivity = `
-            <article class="postActivity">
-                <article class="postActivityLink like-button" data-post-id="${post._id}">
-                    <i class='bx bx-like bx-flip-horizontal'></i>&nbsp;<span>Like</span>
-                </article>
-                <article class="postActivityLink">
-                    <i class='bx bx-comment-detail'></i>&nbsp;<span>Comment</span>
-                </article>
-                <article class="postActivityLink">
-                    <i class='bx bx-repost'></i>&nbsp;<span>Repost</span>
-                </article>
-                <article class="postActivityLink">
-                    <i class='bx bxs-send'></i>&nbsp;<span>Send</span>
-                </article>
-            </article>`;
+      <article class="postActivity">
+          <article class="postActivityLink like-button" data-post-id="${post._id}">
+              <i class='bx bx-like bx-flip-horizontal'></i>&nbsp;<span>Like</span>
+          </article>
+          <article class="postActivityLink">
+              <i class='bx bx-comment-detail'></i>&nbsp;<span>Comment</span>
+          </article>
+          <article class="postActivityLink">
+              <i class='bx bx-repost'></i>&nbsp;<span>Repost</span>
+          </article>
+          <article class="postActivityLink">
+              <i class='bx bxs-send'></i>&nbsp;<span>Send</span>
+          </article>
+      </article>`;
 
     postElement.innerHTML =
       postAuthor + postContent + postImage + postStats + postActivity;
@@ -159,5 +164,38 @@ async function removeLike(postId) {
   }
 }
 
-fetchPosts();
+async function createPost(event) {
+  event.preventDefault();
 
+  const input = document.querySelector(".createPostInput input");
+  const postText = input.value.trim();
+
+  if (!postText) {
+    alert("Post content cannot be empty!");
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      "http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ text: postText }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to create post");
+    }
+
+    const newPost = await response.json();
+    input.value = "";
+    fetchPosts(); // Refresh the posts to include the new post
+  } catch (error) {
+    console.error("Error creating post:", error);
+  }
+}
